02209db2728eae0fee57a0538450fefd1c793131
jackasoft <njengaeliud85@gmail.com>
2023-04-05 15:05:30 +0300

    Student App

---
+++ app/src/main/java/dev/students/app_tab/MainActivity.java
@@ -0,0 +1,28 @@
+package dev.students.app_tab;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import dev.students.app_tab.databinding.ActivityMainBinding;
+import dev.students.app_tab.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+    }
+}
